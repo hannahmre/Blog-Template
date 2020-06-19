bad3ffbbb3927450878d28c8f415f1fbed955bc0
hannahmre <hcm8434@uncw.edu>
2020-06-19 19:20:47 -0400

    Content boxes edit and background change script

---
+++ scripts/transition.js
@@ -0,0 +1,12 @@
+function color_change(tab) {
+    if (tab == 'about') {
+        document.body.style.backgroundColor = "#ffccff";
+    } else if (tab == 'travel') {
+        document.body.style.backgroundColor = "#ccccff";
+    } else if (tab == 'recipes') {
+        document.body.style.backgroundColor = "#ccffff";
+    } else {
+        document.body.style.backgroundColor = "#ccffcc";
+    }
+
+}
