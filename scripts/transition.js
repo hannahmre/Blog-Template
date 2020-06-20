f85241502b6fa5f5c87ad94598cce1ca52d3cd5c
hannahmre <hcm8434@uncw.edu>
2020-06-19 22:07:23 -0400

    Content boxes transition on button click

--- scripts/transition.js
+++ scripts/transition.js
@@ -1,12 +1,28 @@
 function color_change(tab) {
     if (tab == 'about') {
         document.body.style.backgroundColor = "#ffccff";
+        document.getElementById('contentbox1').style.display = 'block';
+        document.getElementById('contentbox2').style.display = 'none';
+        document.getElementById('contentbox3').style.display = 'none';
+        document.getElementById('contentbox4').style.display = 'none';
     } else if (tab == 'travel') {
         document.body.style.backgroundColor = "#ccccff";
+        document.getElementById('contentbox2').style.display = 'block';
+        document.getElementById('contentbox1').style.display = 'none';
+        document.getElementById('contentbox3').style.display = 'none';
+        document.getElementById('contentbox4').style.display = 'none';
     } else if (tab == 'recipes') {
         document.body.style.backgroundColor = "#ccffff";
+        document.getElementById('contentbox3').style.display = 'block';
+        document.getElementById('contentbox2').style.display = 'none';
+        document.getElementById('contentbox1').style.display = 'none';
+        document.getElementById('contentbox4').style.display = 'none';
     } else {
         document.body.style.backgroundColor = "#ccffcc";
+        document.getElementById('contentbox4').style.display = 'block';
+        document.getElementById('contentbox2').style.display = 'none';
+        document.getElementById('contentbox3').style.display = 'none';
+        document.getElementById('contentbox1').style.display = 'none';
     }
 
 }
