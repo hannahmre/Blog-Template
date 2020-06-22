3b272361a9aa8eb7b046800961bb91ca234d99ce
hannahmre <hcm8434@uncw.edu>
2020-06-21 23:35:48 -0400

    simplified2

--- scripts/transition.js
+++ scripts/transition.js
@@ -1,28 +1,43 @@
+var buttons = document.getElementsByClassName = 'tabs';
+
+console.log(buttons[0]);
+console.log(buttons[1]);
+console.log(buttons[2]);
+console.log(buttons[3]);
+
 function color_change(tab) {
+
     if (tab == 'about') {
         document.body.style.backgroundColor = "#ffccff";
-        document.getElementById('contentbox1').style.display = 'block';
-        document.getElementById('contentbox2').style.display = 'none';
-        document.getElementById('contentbox3').style.display = 'none';
-        document.getElementById('contentbox4').style.display = 'none';
+        transition(0);
+
     } else if (tab == 'travel') {
         document.body.style.backgroundColor = "#ccccff";
-        document.getElementById('contentbox2').style.display = 'block';
-        document.getElementById('contentbox1').style.display = 'none';
-        document.getElementById('contentbox3').style.display = 'none';
-        document.getElementById('contentbox4').style.display = 'none';
+        console.log('here2');
+        transition(1);
+
     } else if (tab == 'recipes') {
+
         document.body.style.backgroundColor = "#ccffff";
-        document.getElementById('contentbox3').style.display = 'block';
-        document.getElementById('contentbox2').style.display = 'none';
-        document.getElementById('contentbox1').style.display = 'none';
-        document.getElementById('contentbox4').style.display = 'none';
+        transition(2);
+
     } else {
         document.body.style.backgroundColor = "#ccffcc";
-        document.getElementById('contentbox4').style.display = 'block';
-        document.getElementById('contentbox2').style.display = 'none';
-        document.getElementById('contentbox3').style.display = 'none';
-        document.getElementById('contentbox1').style.display = 'none';
+        transition(3);
+
     }
 
+}
+
+function transition(num) {
+    for (var b = 0; b < buttons.length - 1; b++) {
+        if (b == num) {
+            buttons[num].style.display = 'block';
+            buttons[num].classList.add = 'active';
+        } else {
+            buttons[b].style.display = 'none';
+            buttons[b].classList.remove = 'active';
+        }
+
+    }
 }
