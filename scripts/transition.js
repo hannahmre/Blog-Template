4bacaf6f282d5a5b5abce905def5d0c62b5a4187
hannahmre <hcm8434@uncw.edu>
2020-06-24 22:58:33 -0400

    fix travel page & started js for profile

--- scripts/transition.js
+++ scripts/transition.js
@@ -1,45 +1,73 @@
-var buttons = document.getElementsByClassName('tabs');
-var boxes = document.getElementsByClassName('box');
+// var buttons = document.getElementsByClassName('tabs');
+// var boxes = document.getElementsByClassName('box');
 
-console.log(buttons[0]);
-console.log(buttons[1]);
-console.log(buttons[2]);
-console.log(buttons[3]);
+var timing = setInterval(setText, 800);
 
-function color_change(tab) {
+var texts = ["a Blogger", "an Entrepreneur", "a Traveler", "a Cook", "a Health Nut", "a Web Developer"];
 
-    if (tab == 'about') {
-        document.body.style.backgroundColor = "#ffccff";
-        transition(0);
+// function setText() {
+//     while (x == false) {
+//         for (var word = 0; word < texts.length - 1; word++) {
+//             document.getElementById("profileoption").innerHTML = texts[word];
+//         }
+//     }
+// }
 
-    } else if (tab == 'travel') {
-        document.body.style.backgroundColor = "#ccccff";
-        transition(1);
+var prevword;
+var currentword;
 
-    } else if (tab == 'recipes') {
+function setText() {
+    console.log('in here')
+    for (var word = 0; word < texts.length; word++) {
+        // console.log(texts[word])
+        if (prevword != texts[word]) {
+            currentword = texts[word];
+            console.log(currentword)
+            document.getElementById("profileoption").innerHTML = currentword;
+            prevword = texts[word];
 
-        document.body.style.backgroundColor = "#ccffff";
-        transition(2);
-
-    } else {
-        document.body.style.backgroundColor = "#ccffcc";
-        transition(3);
-
-    }
-
-}
-
-function transition(num) {
-    for (var b = 0; b < buttons.length - 1; b++) {
-        if (b == num) {
-            console.log('here1');
-            boxes[num].style.display = 'block';
-            buttons[num].classList.add = 'active';
-            console.log('here2');
         } else {
-            boxes[b].style.display = 'none';
-            buttons[b].classList.remove = 'active';
+            document.getElementById("profileoption").innerHTML = texts[word + 1];
+            prevword = texts[word + 1];
         }
 
     }
 }
+
+// function color_change(tab) {
+
+//     if (tab == 'about') {
+//         document.body.style.backgroundColor = "#ffccff";
+//         transition(0);
+
+//     } else if (tab == 'travel') {
+//         document.body.style.backgroundColor = "#ccccff";
+//         transition(1);
+
+//     } else if (tab == 'recipes') {
+
+//         document.body.style.backgroundColor = "#ccffff";
+//         transition(2);
+
+//     } else {
+//         document.body.style.backgroundColor = "#ccffcc";
+//         transition(3);
+
+//     }
+
+// }
+
+// function transition(num) {
+//     for (var b = 0; b < buttons.length - 1; b++) {
+//         if (b == num) {
+//             console.log('here1');
+//             boxes[num].style.display = 'block';
+//             buttons[num].classList.add = 'active';
+//             console.log('here2');
+//         } else {
+//             boxes[b].style.display = 'none';
+//             buttons[b].classList.remove = 'active';
+//         }
+
+//     }
+// }
